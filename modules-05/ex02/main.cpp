c90ed2acb06fdd5fa510962e07096b278cdf182f
ketzon <bessonflorian42@yahoo.fr>
2024-04-22 17:59:33 +0200

    aligned

--- modules-05/ex02/main.cpp
+++ modules-05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fbesson <fbesson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 14:37:13 by fbesson           #+#    #+#             */
-/*   Updated: 2024/04/22 17:57:18 by fbesson          ###   ########.fr       */
+/*   Updated: 2024/04/22 17:59:05 by fbesson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,27 @@
 
 int main(void) 
 {
-    Bureaucrat bob("bob", 10);
-    ShrubberyCreationForm a("home");   // 145, 137
-    RobotomyRequestForm b("wall-e");    // 72, 45
-    PresidentialPardonForm c("douglas adams");  // 25, 5
-	
+	Bureaucrat bob("bob", 10);
+	ShrubberyCreationForm a("home");   // 145, 137
+	RobotomyRequestForm b("wall-e");    // 72, 45
+	PresidentialPardonForm c("douglas adams");  // 25, 5
+
 	std::cout << "\n---Bob---" << std::endl;
 	bob.executeForm(a); //throw exception [not signed]
 
-    bob.signForm(a);
-    bob.signForm(b);
-    bob.signForm(c);
+	bob.signForm(a);
+	bob.signForm(b);
+	bob.signForm(c);
 
-    bob.executeForm(a);
-    bob.executeForm(b);
-    bob.executeForm(c);  // throw exception [exec grade to low]
+	bob.executeForm(a);
+	bob.executeForm(b);
+	bob.executeForm(c);  // throw exception [exec grade to low]
 
 	std::cout << "\n---Boss---" << std::endl;
-    Bureaucrat boss("big brother", 1);
-    boss.executeForm(a);
-    boss.executeForm(b);
-    boss.executeForm(c);
+	Bureaucrat boss("big brother", 1);
+	boss.executeForm(a);
+	boss.executeForm(b);
+	boss.executeForm(c);
 
 	return 0;
 }
