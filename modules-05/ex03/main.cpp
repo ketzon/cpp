6fbecd6fa8841435b75ad752acf7b5a267f4b328
ketzon <bessonflorian42@yahoo.fr>
2024-04-23 10:07:48 +0200

    fdfsd

--- modules-05/ex03/main.cpp
+++ modules-05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fbesson <fbesson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 14:37:13 by fbesson           #+#    #+#             */
-/*   Updated: 2024/04/22 21:24:59 by fbesson          ###   ########.fr       */
+/*   Updated: 2024/04/23 10:06:39 by fbesson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,39 @@
 int main(void) 
 {
 	Bureaucrat boss("boss", 1);
-	AForm *ptr;
+	AForm *scf, *rrf, *ppr, *wrong;
 	Intern intern;
-	ptr = intern.makeForm("PresidentialPardonForm", "bobby");
-	if (ptr)
+	std::cout << "\n---ShrubberyCreationForm test---" << std::endl;
+	scf = intern.makeForm("ShrubberyCreationForm", "Home");
+	if (scf)
 	{
-		boss.signForm(*ptr);
-		boss.executeForm(*ptr);
+		boss.signForm(*scf);
+		boss.executeForm(*scf);
 	}
-	delete ptr;
+	std::cout << "\n---RobotomyRequestForm test---" << std::endl;
+	rrf = intern.makeForm("RobotomyRequestForm", "WALL-E");
+	if (rrf)
+	{
+		boss.signForm(*rrf);
+		boss.executeForm(*rrf);
+	}
+	std::cout << "\n---PresidentialPardonForm test---" << std::endl;
+	ppr = intern.makeForm("PresidentialPardonForm", "Douglas Adams");
+	if (ppr)
+	{
+		boss.signForm(*ppr);
+		boss.executeForm(*ppr);
+	}
+	std::cout << "\n---WrongForm test---" << std::endl;
+	wrong = intern.makeForm("wrong name", "wrong target");
+	if (wrong)
+	{
+		boss.signForm(*wrong);
+		boss.executeForm(*wrong);
+	}
+	delete scf;
+	delete rrf;
+	delete ppr;
+	delete wrong;
 	return 0;
 }
